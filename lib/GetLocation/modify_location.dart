ddeb37e6587be4dc001359b01a052b4802bcbd3c
sonyyh <sy3hong@gmail.com>
2025-04-06 00:01:35 -0700

    set up geolocator using yt tutorial

---
+++ lib/GetLocation/modify_location.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:geolocator/geolocator.dart';
+
+class ModifyLocation extends StatefulWidget {
+  const ModifyLocation({super.key});
+
+  @override
+  State<ModifyLocation> createState() => _ModifyLocationState();
+}
+
+class _ModifyLocationState extends State<ModifyLocation> {
+
+  String _latitude = 'Latitude';
+  String _longitude = 'Longitude';
+
+  Future<void> fetchCurrentGeopostion() async 
+  {
+    try 
+    {
+      final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+      );
+      
+      Position currentposition = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+      
+      setState(() {
+        _latitude = 'Latitude: ${currentposition.latitude}';
+        _longitude = 'Longitude: ${currentposition.longitude}';
+      });
+    } catch (error) 
+    {
+      // ignore: avoid_print
+      print('Error: $error');
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GeoLocator'),
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: Colors.blue))
+      ),
+    );
+  }
+}
